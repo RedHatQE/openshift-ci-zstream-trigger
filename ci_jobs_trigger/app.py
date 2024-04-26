6a2cb338df3170d4995f9bcf39ca2482ee72b603
Meni Yakove <myakove@gmail.com>
2024-04-26 14:35:37 +0300

    Add more logs for jira actions

--- ci_jobs_trigger/app.py
+++ ci_jobs_trigger/app.py
@@ -90,7 +90,6 @@ def process_addons_trigger():
 
 if __name__ == "__main__":
     APP.logger.info(f"Starting {APP.name} app")
-    APP.logger.info(f"Starting {APP.name} app")
 
     run_in_process(
         targets={
